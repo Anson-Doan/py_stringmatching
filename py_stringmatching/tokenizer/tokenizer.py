39f0656cf027bcc1d21b9204b0a7087cfe4a88e3
Anson-Doan <anson.ddoan@gmail.com>
2016-06-21 11:00:34 -0500

    Add files via upload

--- py_stringmatching/tokenizer/tokenizer.py
+++ py_stringmatching/tokenizer/tokenizer.py
@@ -6,9 +6,12 @@ class Tokenizer(object):
     Parameters:
         return_set (boolean): an attribute which is a flag to indicate whether to return a set of
                               tokens instead of a bag of tokens (defaults to False).
+        is_lowercase (boolean): an attribute which is a flag to indicate whether to convert the input
+                              entirely into lowercase (defaults to False).
     """
-    def __init__(self, return_set=False):
+    def __init__(self, return_set=False, is_lowercase=False):
         self.return_set = return_set
+        self.is_lowercase = is_lowercase
 
     def get_return_set(self):
         """Get the return_set flag.
@@ -25,4 +28,21 @@ class Tokenizer(object):
             return_set (boolean): flag to indicate whether to return a set of tokens or a bag of tokens. 
         """
         self.return_set = return_set
+        return True
+
+    def get_is_lowercase(self):
+        """Get the is_lowercase flag.
+
+        Returns:
+            The boolean value of the is_lowercase attribute.
+        """
+        return self.is_lowercase
+
+    def set_return_set(self, is_lowercase):
+        """Set the is_lowercase flag.
+
+        Args:
+            return_set (boolean): flag to indicate whether to convert input to all lowercase.
+        """
+        self.is_lowercase = is_lowercase
         return True
